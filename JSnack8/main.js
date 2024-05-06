ee1b01a53dcb956dec840ae3be34fc1387d60ca3
gianluigiricciolio <gianluigiricciolio@proton.me>
2024-05-06 09:22:47 +0200

    [UPD] added control on input type

--- JSnack8/main.js
+++ JSnack8/main.js
@@ -3,13 +3,18 @@
 const digitsNumber = 4;
 let number = "";
 let digitsSum = 0;
+let buffer;
 
 while(number.length!=4){
-    number=prompt(`Inserisci un numero di ${digitsNumber} cifre`);
-    if (number.length == digitsNumber){
-        for(let i=0; i < digitsNumber; i++){
-            digitsSum+=Number(number[i]);
-        }
-    } else console.log(`Non sono ${digitsNumber} cifre`);
+    buffer=prompt(`Inserisci un numero di ${digitsNumber} cifre`);
+    if (!isNaN(buffer)){
+        if (buffer.length == digitsNumber){
+            number=buffer;
+            for(let i=0; i < digitsNumber; i++){
+                digitsSum+=Number(number[i]);
+            }
+        } else console.log(`Non sono ${digitsNumber} cifre`);
+    } else console.log('Non sono numeri');
+    
 }
-console.log(digitsSum);
+console.log(`La loro somma vale ${digitsSum}`);
